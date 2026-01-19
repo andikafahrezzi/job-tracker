4ef2f76a9e7cca84b9e8280b56ac76ea12cdc92d
addust <andikafahrezi5@gmail.com>
2026-01-19 14:30:12 +0700

    style: change style welcome, login and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar - Job Tracker</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="h-full">
     <div class="min-h-full flex">
