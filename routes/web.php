213e1d50cca9053b6e0821a1e79b6aee3cd2098b
addust <andikafahrezi5@gmail.com>
2026-01-25 17:03:11 +0700

    fix: fix route conflict (application.update

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/applications/create', [ApplicationController::class, 'create'])->name('applications.create');
     Route::post('/applications', [ApplicationController::class, 'store'])->name('applications.store');
     Route::get('/applications/{application}/edit', [ApplicationController::class, 'edit'])->name('applications.edit');
-    Route::put('/applications/{application}', [ApplicationController::class, 'update'])->name('applications.update');
     Route::delete('/applications/{application}', [ApplicationController::class, 'destroy'])->name('applications.destroy');
 });
 
